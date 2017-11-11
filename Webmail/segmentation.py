9a4be53729099b8deea7c42610c289dd99d4c41f
Pratik Bhangale <pratik@localhost.localdomain>
2017-11-12 01:10:31 +0530

    smooth added

--- Webmail/segmentation.py
+++ Webmail/segmentation.py
@@ -4,6 +4,7 @@ import sys
 import math
 from matplotlib import pyplot as plt
 import string
+import smooth as st
 d = dict.fromkeys(string.ascii_uppercase, 0)
 d1 = dict.fromkeys(string.digits, 0)
 d.update(d1)
@@ -23,15 +24,18 @@ for p in range (1, 2501):
 		img2= cv2.resize(img2,(60,80),interpolation=cv2.INTER_CUBIC)
 		img3 = img[:,(2*diff)+1:(3*diff),:] 
 		img3= cv2.resize(img3,(60,80),interpolation=cv2.INTER_CUBIC)
+		imgg1 = st.smooth(img1)
+		imgg2 = st.smooth(img2)
+		imgg3 = st.smooth(img3)
 		path  = 'Classes/'+lab[0]+'/'+str(d[lab[0]])+'.jpg'
 		d[lab[0]] += 1
-		cv2.imwrite(path,img1)
+		cv2.imwrite(path,imgg1)
 		path  = 'Classes/'+lab[1]+'/'+str(d[lab[1]])+'.jpg'
 		d[lab[1]] += 1
-		cv2.imwrite(path,img2)
+		cv2.imwrite(path,imgg2)
 		path  = 'Classes/'+lab[2]+'/'+str(d[lab[2]])+'.jpg'
 		d[lab[2]] += 1
-		cv2.imwrite(path,img3)
+		cv2.imwrite(path,imgg3)
 	elif break_num == 4: 
 		img1 = img[:,0:diff,:] 
 		img1= cv2.resize(img1,(60,80),interpolation=cv2.INTER_CUBIC)
@@ -41,15 +45,19 @@ for p in range (1, 2501):
 		img3= cv2.resize(img3,(60,80),interpolation=cv2.INTER_CUBIC)
 		img4 = img[:,(3*diff)+1:(4*diff),:] 
 		img4= cv2.resize(img4,(60,80),interpolation=cv2.INTER_CUBIC)
+		imgg1 = st.smooth(img1)
+		imgg2 = st.smooth(img2)
+		imgg3 = st.smooth(img3)
+		imgg4 = st.smooth(img4)
 		path  = 'Classes/'+lab[0]+'/'+str(d[lab[0]])+'.jpg'
 		d[lab[0]] += 1
-		cv2.imwrite(path,img1)
+		cv2.imwrite(path,imgg1)
 		path  = 'Classes/'+lab[1]+'/'+str(d[lab[1]])+'.jpg'
 		d[lab[1]] += 1
-		cv2.imwrite(path,img2)
+		cv2.imwrite(path,imgg2)
 		path  = 'Classes/'+lab[2]+'/'+str(d[lab[2]])+'.jpg'
 		d[lab[2]] += 1
-		cv2.imwrite(path,img3)
+		cv2.imwrite(path,imgg3)
 		path  = 'Classes/'+lab[3]+'/'+str(d[lab[3]])+'.jpg'
 		d[lab[3]] += 1
-		cv2.imwrite(path,img4)
+		cv2.imwrite(path,imgg4)
---
+++ Webmail/train_web_gray.py
@@ -0,0 +1,152 @@
+import numpy as np
+import cv2
+import sys
+import math
+from matplotlib import pyplot as plt
+import string
+from os import listdir
+from os.path import isfile, join
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data as data_utils 
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from model import *
+
+
+
+
+d = dict.fromkeys(string.ascii_uppercase,[])
+d1 = dict.fromkeys(string.digits,[])
+d.update(d1)
+cnt=0
+length=0
+for key in d.keys():
+	mypath = 'Classes/'+key+'/';
+	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	length+=len(onlyfiles)
+	#print(onlyfiles)
+	d[key] = onlyfiles
+print(length)
+inp=np.ones((length,1,80,60))
+target=np.zeros((length,))
+i=0
+for key in d.keys():
+	lst = []
+	for element in d[key]:
+		temp_path='Classes/'+key+'/'+ element
+		lst.append(temp_path)
+		temp=cv2.imread(temp_path,0)
+		#print(temp.shape)
+		# temp= cv2.resize(temp,(40,40),interpolation=cv2.INTER_CUBIC)
+		#temp=temp.ravel()
+		#print(type(temp),temp.shape,inp.shape,type(inp))
+		#print(inp.shape)
+		inp[i][0]=temp
+		if (ord(key)<65):
+			num=ord(key)-48
+		else:
+			num=ord(key)-55
+		# target[i][num]=1
+		target[i]=num
+		i+=1
+				#print(inp.shape,target.shape)
+	cnt+=1
+	d[key] = lst
+	print(key,inp.shape)
+
+#inp=np.swapaxes(inp,1,3)
+features=torch.from_numpy(inp)
+targets=torch.from_numpy(target)
+#target=target.reshape((target.shape[0],))
+#print(inp.shape,target.shape,type(data_utils.TensorDataset))
+train = data_utils.TensorDataset(features, targets) 
+train_loader = data_utils.DataLoader(train, batch_size=20, shuffle=True)
+
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(10):  # loop over the dataset multiple times
+
+	running_loss = 0.0
+	for i, data in enumerate(train_loader, 0):
+		# get the inputs
+		inputs, labels = data
+
+		# wrap them in Variable
+		inputs, labels = Variable(inputs), Variable(labels)
+		#print(labels)
+
+		# zero the parameter gradients
+		optimizer.zero_grad()
+
+		# forward + backward + optimize
+		
+		outputs = net(inputs.float())
+		loss = criterion(outputs, labels.long())
+		loss.backward()
+		optimizer.step()
+
+		# print statistics
+		running_loss += loss.data[0]
+		if i % 10 == 9:    # print every 2000 mini-batches
+			print('[%d, %5d] loss: %.3f' %
+				  (epoch + 1, i + 1, running_loss / 10))
+			running_loss = 0.0
+
+print('Finished Training')
+torch.save(net, 'trainmodel_web.pt')
+
+test_loader=train_loader
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+# images=images.double()
+# labels=labels.long()
+outputs = net(Variable(images).float())
+correct = 0
+total = 0
+for data in test_loader:
+    images, labels = data
+    outputs = net(Variable(images).float())
+    _, predicted = torch.max(outputs.data, 1)
+    labels=labels.long()
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+
+print('Training Accuracy: %d %%' % (
+    100 * correct / total))
+
+
+
+class_correct = np.zeros(36,)
+class_total = np.zeros(36,)
+for data in test_loader:
+    images, labels = data
+    outputs = net(Variable(images).float())
+    _, predicted = torch.max(outputs.data, 1)
+    #print(predicted.numpy().ravel(),labels.numpy())
+    d=predicted.numpy().ravel()
+    q=labels.numpy()
+    #print("hdvbvjs")
+    c = 1*((d==q).squeeze())
+    #print(c)
+    for i in range(0,len(c)):
+        label = int(q[i])
+        #print(label)
+        class_correct[label] += c[i]
+        class_total[label] += 1
+
+print(class_correct,class_total)
+
+for i in range(1,36):
+    if(class_total[i]!=0):
+        if (i<10):
+            print('Accuracy of %5s : %2d %%' % (i, 100 * class_correct[i] / class_total[i]))
+        else:
+            print('Accuracy of %5s : %2d %%' % (chr(i+55), 100 * class_correct[i] / class_total[i]))
